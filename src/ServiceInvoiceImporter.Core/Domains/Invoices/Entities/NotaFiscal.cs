cfbe8486a8c7e364a566804c29736f25fc5e44b6
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-09-27 17:09:17 -0300

    chore: develop entity NotaFiscal domain class

---
+++ src/ServiceInvoiceImporter.Core/Domains/Invoices/Entities/NotaFiscal.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceInvoiceImporter.Core.Domains.Invoices.Entities;
+
+public class NotaFiscal
+{
+    public int Id { get; set; }
+    public int Numero { get; set; }
+    public string CNPJPrestador { get; set; } = string.Empty;
+    public string CNPJTomador { get; set; } = string.Empty;
+    public DateOnly DataEmissao { get; set; }
+    public string DescricaoServico { get; set; } = string.Empty;
+    public decimal ValorTotal { get; set; }
+    public int MyProperty { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+}
